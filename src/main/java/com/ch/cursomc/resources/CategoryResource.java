998462c339ad749fb8aa6cc0b65036c4167ab7f1
Carlos H Lopes <correioch@gmail.com>
2018-12-23 19:32:41 -0500

    Creation et test classe Category

--- src/main/java/com/ch/cursomc/resources/CategoryResource.java
+++ src/main/java/com/ch/cursomc/resources/CategoryResource.java
@@ -1,16 +1,35 @@
 package com.ch.cursomc.resources;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ch.cursomc.domain.Category;
+
 @RestController
 @RequestMapping(value="/categories")
-public class CategoryResource {
+public class CategoryResource implements Serializable {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@RequestMapping(method=RequestMethod.GET)
-	public String list() {
-		return "Rest ca marche";
+	public List<Category> list() {
+		
+		Category cat1 = new Category(1, "Informatique");
+		Category cat2 = new Category(1, "Marketinig");
+		
+		List <Category> listCategory = new ArrayList<>();
+		listCategory.add(cat1);
+		listCategory.add(cat2);
+		
+		return listCategory;
 	}
 
 }
