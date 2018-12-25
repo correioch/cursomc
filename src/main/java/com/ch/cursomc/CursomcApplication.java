57312183654170ec2945e0f8e182ab1c7b86a8ae
Carlos H Lopes <correioch@gmail.com>
2018-12-25 16:05:16 -0500

    Creation d'instance automatique

--- src/main/java/com/ch/cursomc/CursomcApplication.java
+++ src/main/java/com/ch/cursomc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.ch.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ch.cursomc.domain.Category;
+import com.ch.cursomc.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+    private	CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Informatique");
+		Category cat2 = new Category(null, "Bureau 2");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
