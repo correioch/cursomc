bfdb793660634b364a2679f0aabc67104a103175
Carlos H Lopes <correioch@gmail.com>
2018-12-25 15:03:57 -0500

    Creation Category Service et repository

--- src/main/java/com/ch/cursomc/resources/CategoryResource.java
+++ src/main/java/com/ch/cursomc/resources/CategoryResource.java
@@ -1,35 +1,30 @@
 package com.ch.cursomc.resources;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ch.cursomc.domain.Category;
+import com.ch.cursomc.services.CategoryService;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource implements Serializable {
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
+	@Autowired
+	private CategoryService service;
 
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Category> list() {
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		Category cat1 = new Category(1, "Informatique");
-		Category cat2 = new Category(1, "Marketinig");
-		
-		List <Category> listCategory = new ArrayList<>();
-		listCategory.add(cat1);
-		listCategory.add(cat2);
-		
-		return listCategory;
+		Category obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
---
+++ src/main/java/com/ch/cursomc/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.ch.cursomc.services;
+
+import java.util.Optional;
+
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ch.cursomc.domain.Category;
+import com.ch.cursomc.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repo; 
+	
+	public Category find(Integer id) {
+		
+		Optional<Category> obj = repo.findById(id);
+		return obj.orElseThrow(() -> 
+			new ObjectNotFoundException("Objet pas trouve! id: " + id + "Tipo: ", 
+				Category.class.getName()));
+	}
+	
+}
